a247f1fa9ef0ef647298b7597250fb6ca7353714
Kramskiy Dima <64987701+ifinddima@users.noreply.github.com>
2020-10-07 22:58:15 +0300

    решение задачи-04

---
+++ js/task-04.js
@@ -0,0 +1,25 @@
+const countTotalSalary = function (employees) {
+  // Write code under this line
+  let total = 0;
+  for (const key in employees) {
+    total += employees[key];
+  }
+  return total;
+};
+
+// Объекты и ожидаемый результат
+const developers = {
+  mango: 300,
+  poly: 250,
+  alfred: 450,
+};
+console.log(countTotalSalary(developers));
+// 1000
+
+const supports = {
+  kiwi: 200,
+  lux: 150,
+  chelsy: 150,
+};
+console.log(countTotalSalary(supports));
+// 500
